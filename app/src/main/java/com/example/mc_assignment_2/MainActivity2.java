0019d8701d3b2a7166019ae8001dc50961859e02
Usama Ijaz <usamaijaz2001@gmail.com>
2022-05-22 18:24:33 +0500

    Completed second activity code.

--- app/src/main/java/com/example/mc_assignment_2/MainActivity2.java
+++ app/src/main/java/com/example/mc_assignment_2/MainActivity2.java
@@ -14,6 +14,40 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-     
+        iv=findViewById(R.id.iv);
+        tv=findViewById(R.id.textView2);
+        Intent intent=getIntent();
+        String msg= intent.getStringExtra(MainActivity.str);
+        tv.setText(msg);
+        if(msg.equals("1")){
+            iv.setImageResource(R.drawable.one);
+        }
+        if(msg.equals("2")){
+            iv.setImageResource(R.drawable.two);
+        }
+        if(msg.equals("3")){
+            iv.setImageResource(R.drawable.three);
+        }
+        if(msg.equals("4")){
+            iv.setImageResource(R.drawable.four);
+        }
+        if(msg.equals("5")){
+            iv.setImageResource(R.drawable.five);
+        }
+        if(msg.equals("6")){
+            iv.setImageResource(R.drawable.six);
+        }
+        if(msg.equals("7")){
+            iv.setImageResource(R.drawable.seven);
+        }
+        if(msg.equals("8")){
+            iv.setImageResource(R.drawable.eight);
+        }
+        if(msg.equals("9")){
+            iv.setImageResource(R.drawable.nine);
+        }
+        if(msg.equals("10")){
+            iv.setImageResource(R.drawable.ten);
+        }
     }
 }
